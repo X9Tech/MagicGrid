aecef25238a17082fadb1873718f127643dcc20e
Brandon Potter <brandon@x9tech.com>
2015-11-20 13:57:40 -0500

    Make compatible with .net 4

--- MagicGrid/MagicGridControls/Properties/Resources.Designer.cs
+++ MagicGrid/MagicGridControls/Properties/Resources.Designer.cs
@@ -9,6 +9,7 @@
 //------------------------------------------------------------------------------
 
 namespace MagicGridControls.Properties {
+    using System;
     
     
     /// <summary>
@@ -37,7 +38,7 @@ namespace MagicGridControls.Properties {
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
-                if ((resourceMan == null)) {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MagicGridControls.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
